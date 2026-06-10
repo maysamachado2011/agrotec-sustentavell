c1a3bda077d39e97a7648b4747371545285d27f3
maysamachado2011 <maysa.ribeiro.machado@escola.pr.gov.br>
2026-06-10 15:25:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+function showInfo(topic) {
+  const infoBox = document.getElementById('info-box');
+  let content = '';
 
+  switch(topic) {
+    case 'agrotech':
+      content = 'AgroTech: uso de drones e IA para aumentar produtividade.';
+      break;
+    case 'reforest':
+      content = 'Reflorestamento: recuperação de áreas degradadas.';
+      break;
+    case 'organic':
+      content = 'Orgânicos: cultivo sem químicos.';
+      break;
+  }
+
+  infoBox.innerHTML = `<p>${content}</p>`;
+}
+
+document.getElementById('contactForm').addEventListener('submit', function(e){
+  e.preventDefault();
+  alert('Mensagem enviada! Obrigado pelo contato.');
+  this.reset();
+});
